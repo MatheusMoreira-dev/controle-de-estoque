73f7d45fd0d352bb04d09e6a510cf447f763d160
Marketing Apoio <marketingapoio@unimedce.com.br>
2025-11-25 13:51:44 -0300

    função para entrega de sessão sob demanda

--- db.py
+++ db.py
@@ -16,7 +16,27 @@ sqlite_file_name = "database.db"
 sqlite_url = f"sqlite:///{sqlite_file_name}"
 
 engine = create_engine(sqlite_url, echo= True)
-session = Session(engine)
+
+# Obter uma sessão sob demanda (yield)
+def get_session():
+    # Cria uma sessão
+    session = Session(engine)
+    
+    try:
+        # Retorne a sessão e pause a execução da função
+        yield session
+
+        # Retorna após o uso
+        # Realiza o commit 
+        session.commit()
+    except Exception:
+        # Desfaz todas as alterações feitas antes do erro
+        session.rollback()
+        # Lança o erro para a rota que retorna o status code  
+        raise
+    finally:
+        # Independente do que acontecer, fecha a sessão
+        session.close()
 
 # Criar as tabelas e o banco
 def create_db_and_tables():

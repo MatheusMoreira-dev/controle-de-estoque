003d7a57349e68ff67035723119ee6d410e55856
Marketing Apoio <marketingapoio@unimedce.com.br>
2025-11-25 12:32:45 -0300

    deletando o arquivo do banco e organizando  o arquivo db.py

--- db.py
+++ db.py
@@ -2,21 +2,23 @@
     1° - Importa as classes do "models" e salva todas com "table=True" no "metadata" 
     da classe mãe SQLModel
 
-    2° - Cria a tabela das respectivas classes salvas no metadata
-    com SQLModel.metadata.create_all(engine) 
+    2° - Cria as tabelas e o banco apenas se o arquivo for executado
 """
 
-# Importe o SQLModel para salvar o metadata e o create_engine para criar a conexão
-from sqlmodel import SQLModel, create_engine
+# Importa o SQLModel, atualizando o metadata
+from sqlmodel import SQLModel, create_engine, Session
 
-# Importe todas as classes da pasta "models", uma por uma
-from models import item, teste
+# Importe as classes da pasta "models", uma por uma
+# Com isso, salva cada uma no metadata
+from models import item
 
 sqlite_file_name = "database.db"
 sqlite_url = f"sqlite:///{sqlite_file_name}"
 
 engine = create_engine(sqlite_url, echo= True)
+session = Session(engine)
 
+# Criar as tabelas e o banco
 def create_db_and_tables():
     SQLModel.metadata.create_all(engine)
 
